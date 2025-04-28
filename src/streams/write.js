ae3fdb37fa3780173542ba45d34561d86e4d4c8f
aleksawsea <alexandrapynzar57@gmail.com>
2025-04-29 02:01:42 +0400

    feat: implement write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,27 @@
+import { createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { stdin as input } from 'node:process';
+import { createInterface } from 'node:readline';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream(filePath);
+  const rl = createInterface({ input });
+  console.log('Hi there! Please, write your content for the file. To terminate it, use Ctrl+C combination :)');
+  rl.on('line', (line) => {
+    stream.write(line + '\n');
+  });
+
+  process.on('SIGINT', () => {
+    rl.close();
+  });
+
+  rl.on('error', (err) => {
+    console.log(err);
+  });
 };
 
 await write();
