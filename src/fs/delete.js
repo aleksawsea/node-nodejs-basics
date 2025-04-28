d2953b03e0f8d36cd6bd88d97ae6eeb1bb4e8e99
aleksawsea <alexandrapynzar57@gmail.com>
2025-04-28 20:15:30 +0400

    feat: implement remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { rm } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filename = path.join(__dirname, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await rm(__filename);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+        throw new Error('FS operation failed');
+    } else {
+        console.log(err.message);
+    }
+  }
 };
 
 await remove();
